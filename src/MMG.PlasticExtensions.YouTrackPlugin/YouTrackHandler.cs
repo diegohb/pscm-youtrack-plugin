31e6106480b6d0446eb0a2a9ca7241a1a4072c14
bustamd1 <bustamd1@.(none)>
2015-01-10 15:38:46 -0500

    -refactored auth method. -added retry logic if auth error. -added logging

--- src/MMG.PlasticExtensions.YouTrackPlugin/YouTrackHandler.cs
+++ src/MMG.PlasticExtensions.YouTrackPlugin/YouTrackHandler.cs
@@ -15,26 +15,13 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
     {
         private static readonly ILog _log = LogManager.GetLogger("extensions");
         private readonly YouTrackExtensionConfiguration _config;
-        private readonly string _authData;
+        private string _authData;
+        private int _authRetryCount = 0 ;
 
         public YouTrackHandler(YouTrackExtensionConfiguration pConfig)
         {
             _config = pConfig;
-            using (var client = new WebClient())
-            {
-                var requestURL = string.Format
-                    ("{0}/rest/user/login?login={1}&password={2}", GetBaseURL(),_config.Username, _config.Password);
-                try
-                {
-                    var result = client.UploadString(requestURL, "POST", "");
-                    if (result == @"<login>ok</login>")
-                        _authData = client.ResponseHeaders.Get("Set-Cookie");
-                }
-                catch (WebException exWeb)
-                {
-                    _log.Error(string.Format("Failed to authenticate using request '{0}'.", requestURL), exWeb);
-                }
-            }
+            authenticate();
         }
 
         public PlasticTask GetPlasticTaskFromTaskID(string pTaskID)
@@ -57,6 +44,15 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
                 }
                 catch (WebException exWeb)
                 {
+                    if (exWeb.Message.Contains("Unauthorized.") && _authRetryCount < 3)
+                    {
+                        _log.WarnFormat
+                            ("YouTrackHandler: Failed to fetch youtrack issue '{0}' due to authentication error. Will retry after authentication again. Details: {1}",
+                                pTaskID, exWeb);
+                        authenticate();
+                        return GetPlasticTaskFromTaskID(pTaskID);
+                    }
+
                     _log.WarnFormat("YouTrackHandler: Failed to find youtrack issue '{0}' due to {1}", pTaskID, exWeb);
                 }
             }
@@ -71,10 +67,40 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
             return string.Format("{0}://{1}", protocol, serverHost);
         }
 
+        #region Support Methods
+
         private static string getTextFromXPathElement(XmlDocument pXMLDoc, string pFieldName)
         {
             var node = pXMLDoc.SelectSingleNode(string.Format("//field[@name='{0}']/value", pFieldName));
             return node != null ? node.InnerText : string.Empty;
         }
+
+        private void authenticate()
+        {
+            _authRetryCount++;
+
+            using (var client = new WebClient())
+            {
+                var requestURL = string.Format
+                    ("{0}/rest/user/login?login={1}&password={2}", GetBaseURL(), _config.Username, _config.Password);
+                try
+                {
+                    var result = client.UploadString(requestURL, "POST", "");
+                    if (result == @"<login>ok</login>")
+                    {
+                        _authData = client.ResponseHeaders.Get("Set-Cookie");
+                        _log.DebugFormat("YouTrackHandler: Successfully authenticated in {0} attempt(s).", _authRetryCount);
+                        _authRetryCount = 0;
+                    }
+                }
+                catch (WebException exWeb)
+                {
+                    _log.Error(string.Format("YouTrackHandler: Failed to authenticate using request '{0}'.", requestURL), exWeb);
+                }
+            }
+        }
+
+        #endregion
+
     }
 }
