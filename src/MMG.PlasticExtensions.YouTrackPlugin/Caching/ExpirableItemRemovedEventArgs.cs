eb2808a71295228f5ea15b31cd5770ab08cd52bd
bustamd1 <bustamd1@.(none)>
2015-01-10 15:05:50 -0500

    added code for caching

---
+++ src/MMG.PlasticExtensions.YouTrackPlugin/Caching/ExpirableItemRemovedEventArgs.cs
@@ -0,0 +1,24 @@
+﻿// *************************************************
+// MMG.PlasticExtensions.YouTrackPlugin.ExpirableItemRemovedEventArgs.cs
+// Last Modified: 01/10/2015 3:02 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace ExpirableDictionary
+{
+    using System;
+
+    /// <summary>
+    /// Contains the key/value pair of the item that expired and has been removed from the dictionary.
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="T"></typeparam>
+    /// <remarks>
+    /// see http://www.jondavis.net/techblog/post/2010/08/30/Four-Methods-Of-Simple-Caching-In-NET.aspx
+    /// </remarks>
+    public class ExpirableItemRemovedEventArgs<K, T> : EventArgs
+    {
+        public K Key { get; set; }
+        public T Value { get; set; }
+    }
+}
