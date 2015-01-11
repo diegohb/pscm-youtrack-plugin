aa8425d25c77485c4c38e9e96cd595fda928bfb1
bustamd1 <bustamd1@.(none)>
2015-01-10 20:59:44 -0500

    code cleanup

--- src/MMG.PlasticExtensions.YouTrackPlugin/YouTrackExtension.cs
+++ src/MMG.PlasticExtensions.YouTrackPlugin/YouTrackExtension.cs
@@ -1,35 +1,35 @@
 // *************************************************
 // MMG.PlasticExtensions.YouTrackPlugin.YouTrackExtension.cs
-// Last Modified: 11/05/2012 11:30 AM
+// Last Modified: 01/10/2015 8:53 PM
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
-using System;
-using System.Collections.Generic;
-using System.Web;
-using Codice.Client.Extension;
-
-using log4net;
-
 namespace MMG.PlasticExtensions.YouTrackPlugin
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using Codice.Client.Extension;
+    using log4net;
+
     public class YouTrackExtension : BasePlasticExtension
     {
         private static readonly ILog _log = LogManager.GetLogger("extensions");
-        private YouTrackExtensionConfiguration _config;
-        private string _configFile = "youtrackextension.conf";
-        private YouTrackHandler _handler;
+        private readonly YouTrackExtensionConfiguration _config;
+        private const string configFile = "youtrackextension.conf";
+        private readonly YouTrackHandler _handler;
 
         public YouTrackExtension()
         {
             try
             {
-                bool bSuccess = false;
-                _config = (YouTrackExtensionConfiguration)ExtensionServices.LoadConfig(_configFile, typeof(YouTrackExtensionConfiguration), out bSuccess);
+                bool bSuccess;
+                _config =
+                    (YouTrackExtensionConfiguration) ExtensionServices.LoadConfig(configFile, typeof (YouTrackExtensionConfiguration), out bSuccess);
                 if (!bSuccess)
                 {
                     _log.WarnFormat
-                        ("YouTrackExtension: Unable to load configuration file: {0}", _configFile);
+                        ("YouTrackExtension: Unable to load configuration file: {0}", configFile);
                     _config = new YouTrackExtensionConfiguration();
                 }
                 else
@@ -37,7 +37,7 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
                     _config.SetDefaultAttributePrefix("yt");
                     mBaseConfig = this._config;
                     _handler = new YouTrackHandler(_config);
-                    _log.InfoFormat("YouTrackExtension: Successfully loaded configuration file: {0}", _configFile);
+                    _log.InfoFormat("YouTrackExtension: Successfully loaded configuration file: {0}", configFile);
                 }
             }
             catch (Exception ex)
@@ -54,8 +54,8 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
         public override void OpenTask(string id, string repName)
         {
             _log.DebugFormat("YouTrackExtension: Open task '{0}'", id);
-            
-            System.Diagnostics.Process.Start(string.Format("{0}/issue/{1}", _handler.GetBaseURL(), id));
+
+            Process.Start(string.Format("{0}/issue/{1}", _handler.GetBaseURL(), id));
         }
 
         public override PlasticTask[] LoadTask(string[] pTaskIDs, string pRepoName)
@@ -87,8 +87,8 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
         {
             return !string.IsNullOrEmpty(_config.BranchPrefix)
                    && pTaskFullName.StartsWith(_config.BranchPrefix, StringComparison.InvariantCultureIgnoreCase)
-                       ? pTaskFullName.Substring(_config.BranchPrefix.Length)
-                       : pTaskFullName;
+                ? pTaskFullName.Substring(_config.BranchPrefix.Length)
+                : pTaskFullName;
         }
     }
 }
