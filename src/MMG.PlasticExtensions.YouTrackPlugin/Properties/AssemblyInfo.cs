2e57fdab023c71f2437f8566c0388bce69b637b1
bustamd1 <bustamd1@.(none)>
2015-01-11 20:33:32 -0500

    changed version to 1.1 and updated assembly info file.

--- src/MMG.PlasticExtensions.YouTrackPlugin/Properties/AssemblyInfo.cs
+++ src/MMG.PlasticExtensions.YouTrackPlugin/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MMG.PlasticExtensions.YouTrackPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("MMG")]
 [assembly: AssemblyProduct("MMG.PlasticExtensions.YouTrackPlugin")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © MMG 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
