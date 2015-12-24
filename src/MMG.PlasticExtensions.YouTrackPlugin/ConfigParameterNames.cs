dfa793d7a1fcfef883895039b01d532b7b2c0b26
bustamd1 <bustamd1@.(none)>
2015-12-24 14:41:44 -0500

    changed constants to public

--- src/MMG.PlasticExtensions.YouTrackPlugin/ConfigParameterNames.cs
+++ src/MMG.PlasticExtensions.YouTrackPlugin/ConfigParameterNames.cs
@@ -1,6 +1,6 @@
 ﻿// *************************************************
 // MMG.PlasticExtensions.YouTrackPlugin.ConfigParameterNames.cs
-// Last Modified: 12/24/2015 2:39 PM
+// Last Modified: 12/24/2015 2:41 PM
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
@@ -8,11 +8,11 @@ namespace MMG.PlasticExtensions.YouTrackPlugin
 {
     public static class ConfigParameterNames
     {
-        internal const string BranchPrefix = "Branch Name Prefix";
-        internal const string UserID = "User ID";
-        internal const string Password = "Password";
-        internal const string Host = "Host";
-        internal const string ShowIssueStateInBranchTitle = "Show issues state in branch title";
-        internal const string ClosedIssueStates = "Issue states considered closed";
+        public const string BranchPrefix = "Branch Name Prefix";
+        public const string UserID = "User ID";
+        public const string Password = "Password";
+        public const string Host = "Host";
+        public const string ShowIssueStateInBranchTitle = "Show issues state in branch title";
+        public const string ClosedIssueStates = "Issue states considered closed";
     }
 }
