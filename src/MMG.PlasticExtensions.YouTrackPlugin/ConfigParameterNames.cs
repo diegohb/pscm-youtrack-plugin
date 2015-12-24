76b5c47f884d225b96311eb18a2c71d9fccfea75
bustamd1 <bustamd1@.(none)>
2015-12-24 14:40:46 -0500

    abstracted config param names class to public class and updated usages

---
+++ src/MMG.PlasticExtensions.YouTrackPlugin/ConfigParameterNames.cs
@@ -0,0 +1,18 @@
+﻿// *************************************************
+// MMG.PlasticExtensions.YouTrackPlugin.ConfigParameterNames.cs
+// Last Modified: 12/24/2015 2:39 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace MMG.PlasticExtensions.YouTrackPlugin
+{
+    public static class ConfigParameterNames
+    {
+        internal const string BranchPrefix = "Branch Name Prefix";
+        internal const string UserID = "User ID";
+        internal const string Password = "Password";
+        internal const string Host = "Host";
+        internal const string ShowIssueStateInBranchTitle = "Show issues state in branch title";
+        internal const string ClosedIssueStates = "Issue states considered closed";
+    }
+}
