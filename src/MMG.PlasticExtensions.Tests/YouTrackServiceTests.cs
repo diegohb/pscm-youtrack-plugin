7c77a08c9b7770937abc82f5eead3da9b9f3312a
bustamd1 <bustamd1@.(none)>
2015-12-24 15:27:02 -0500

    -installed EasyHttp and JsonFX dependencies -implemented ShouldAuthenticateTest -added config for auth user email

---
+++ src/MMG.PlasticExtensions.Tests/YouTrackServiceTests.cs
@@ -0,0 +1,92 @@
+﻿// *************************************************
+// MMG.PlasticExtensions.Tests.YouTrackServiceTests.cs
+// Last Modified: 12/24/2015 3:19 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace MMG.PlasticExtensions.Tests
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using Codice.Client.IssueTracker;
+    using NUnit.Framework;
+    using YouTrackPlugin;
+
+    [TestFixture]
+    public class YouTrackServiceTests
+    {
+        [Test]
+        public void ShouldAuthenticate()
+        {
+            var svc = new YouTrackService(getTestConfig());
+            var user = svc.GetAuthenticatedUser();
+            Assert.IsNotNull(user);
+
+            var expectedValue = ConfigurationManager.AppSettings["test.authUserEmail"];
+            var actualValue = user.Email;
+            Assert.AreEqual(expectedValue, actualValue);
+        }
+
+        private YouTrackExtensionConfigFacade getTestConfig()
+        {
+            var testStoredConfig = new IssueTrackerConfiguration()
+            {
+                WorkingMode = ExtensionWorkingMode.TaskOnBranch,
+                Parameters = getTestConfigParams()
+            };
+            var ytConfig = new YouTrackExtensionConfigFacade(testStoredConfig);
+            return ytConfig;
+        }
+
+        private IssueTrackerConfigurationParameter[] getTestConfigParams()
+        {
+            var parameters = new List<IssueTrackerConfigurationParameter>
+            {
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.BranchPrefix,
+                    Value = "yt",
+                    Type = IssueTrackerConfigurationParameterType.BranchPrefix,
+                    IsGlobal = true
+                },
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.Host,
+                    Value = ConfigurationManager.AppSettings["host"],
+                    Type = IssueTrackerConfigurationParameterType.Host,
+                    IsGlobal = true
+                },
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.UserID,
+                    Value = ConfigurationManager.AppSettings["username"],
+                    Type = IssueTrackerConfigurationParameterType.User,
+                    IsGlobal = false
+                },
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.Password,
+                    Value = ConfigurationManager.AppSettings["password"],
+                    Type = IssueTrackerConfigurationParameterType.Password,
+                    IsGlobal = false
+                },
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.ShowIssueStateInBranchTitle,
+                    Value = "false",
+                    Type = IssueTrackerConfigurationParameterType.Boolean,
+                    IsGlobal = false
+                },
+                new IssueTrackerConfigurationParameter
+                {
+                    Name = ConfigParameterNames.ClosedIssueStates,
+                    Value = "Completed,Approved",
+                    Type = IssueTrackerConfigurationParameterType.Text,
+                    IsGlobal = false
+                }
+            };
+
+            return parameters.ToArray();
+        }
+    }
+}
