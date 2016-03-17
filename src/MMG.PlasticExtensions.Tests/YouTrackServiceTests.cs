c3674a6b8aa24d0da25ae1a7b1018442233fc8fe
greenb1 <greenb1@.(none)>
2016-03-17 11:20:56 -0400

    - Refactor mock creation

--- src/MMG.PlasticExtensions.Tests/YouTrackServiceTests.cs
+++ src/MMG.PlasticExtensions.Tests/YouTrackServiceTests.cs
@@ -1,6 +1,6 @@
 ﻿// *************************************************
 // MMG.PlasticExtensions.Tests.YouTrackServiceTests.cs
-// Last Modified: 03/17/2016 9:28 AM
+// Last Modified: 03/17/2016 11:16 AM
 // Modified By: Green, Brett (greenb1)
 // *************************************************
 
@@ -154,26 +154,32 @@ namespace MMG.PlasticExtensions.Tests
         [Test]
         public void HydratePlasticTask_Base()
         {
+            var facade = GetConfigFacade("http://test.com");
+            facade.SetupGet(x => x.ShowIssueStateInBranchTitle).Returns(false);
+            facade.SetupGet(x => x.IgnoreIssueStateForBranchTitle).Returns("");
+            var sut = new YouTrackService(facade.Object);
+
             dynamic issue = new Issue();
             issue.Id = "ABC1234";
             issue.Summary = "Issue Summary";
             issue.State = "In Progress";
             issue.AssigneeName = "jdoe";
             issue.Description = "Issue Description";
-            var facade = new Mock<IYouTrackExtensionConfigFacade>();
-            facade.SetupGet(x => x.ShowIssueStateInBranchTitle).Returns(false);
-            facade.SetupGet(x => x.IgnoreIssueStateForBranchTitle).Returns("");
-            var uri = new Uri("http://test.com");
-            facade.SetupGet(x => x.Host).Returns(uri);
-            var sut = new YouTrackService(facade.Object);
 
-            //sut.Show
             var task = sut.hydratePlasticTaskFromIssue(issue);
             Assert.AreEqual("ABC1234", task.Id);
             Assert.AreEqual("Issue Summary", task.Title);
             Assert.AreEqual("In Progress", task.Status);
             Assert.AreEqual("jdoe", task.Owner);
             Assert.AreEqual("Issue Description", task.Description);
+        }
+
+        private static Mock<IYouTrackExtensionConfigFacade> GetConfigFacade(string pUri)
+        {
+            var facade = new Mock<IYouTrackExtensionConfigFacade>();
+            var uri = new Uri(pUri);
+            facade.SetupGet(x => x.Host).Returns(uri);
+            return facade;
         }
     }
 }
