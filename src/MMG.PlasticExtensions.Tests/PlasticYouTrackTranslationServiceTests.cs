d0ab7fb3f0681aa4c1e860978ae4cb8f23ef433b
dbustamante@devensoft.com <>
2022-10-17 13:07:43 -0400

    new test fixture

---
+++ src/MMG.PlasticExtensions.Tests/PlasticYouTrackTranslationServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using MMG.PlasticExtensions.YouTrackPlugin;
+using Moq;
+using NUnit.Framework;
+using YouTrackSharp.Issues;
+
+namespace MMG.PlasticExtensions.Tests
+{
+    [TestFixture]
+    public class PlasticYouTrackTranslationServiceTests
+    {
+
+        [Test]
+        public void GetPlasticTaskFromIssue_WithoutUserMapping_ShouldReturnPlasticTaskWithMappedName()
+        {
+            var facade = GetConfigFacade("http://test.com");
+            facade.SetupGet(x => x.ShowIssueStateInBranchTitle).Returns(false);
+            facade.SetupGet(x => x.IgnoreIssueStateForBranchTitle).Returns("");
+            facade.SetupGet(x => x.UsernameMapping).Returns(string.Empty);
+            var sut = new PlasticYouTrackTranslationService(facade.Object);
+
+            dynamic issue = new Issue();
+            issue.Id = "ABC1234";
+            issue.Summary = "Issue Summary";
+            issue.State = "In Progress";
+            issue.Assignee = new List<Assignee>(new[] { new Assignee { UserName = "jdoe", FullName = "John Doe" } });
+            issue.Description = "Issue Description";
+
+            var task = sut.GetPlasticTaskFromIssue(issue);
+            Assert.AreEqual("ABC1234", task.Id);
+            Assert.AreEqual("Issue Summary", task.Title);
+            Assert.AreEqual("In Progress", task.Status);
+            Assert.AreEqual("jdoe", task.Owner);
+            Assert.AreEqual("Issue Description", task.Description);
+        }
+        [Test]
+        public void GetPlasticTaskFromIssue_WithUserMapping_ShouldReturnPlasticTaskWithMappedName()
+        {
+            var facade = GetConfigFacade("http://test.com");
+            facade.SetupGet(x => x.ShowIssueStateInBranchTitle).Returns(false);
+            facade.SetupGet(x => x.IgnoreIssueStateForBranchTitle).Returns("");
+            facade.SetupGet(x => x.UsernameMapping).Returns("john.doe:jdoe");
+            var sut = new PlasticYouTrackTranslationService(facade.Object);
+
+            dynamic issue = new Issue();
+            issue.Id = "ABC1234";
+            issue.Summary = "Issue Summary";
+            issue.State = "In Progress";
+            issue.Assignee = new List<Assignee>(new[] { new Assignee { UserName = "jdoe", FullName = "John Doe" } });
+            issue.Description = "Issue Description";
+
+            var task = sut.GetPlasticTaskFromIssue(issue);
+            Assert.AreEqual("ABC1234", task.Id);
+            Assert.AreEqual("Issue Summary", task.Title);
+            Assert.AreEqual("In Progress", task.Status);
+            Assert.AreEqual("john.doe", task.Owner);
+            Assert.AreEqual("Issue Description", task.Description);
+        }
+
+        private static Mock<IYouTrackExtensionConfigFacade> GetConfigFacade(string pUri)
+        {
+            var facade = new Mock<IYouTrackExtensionConfigFacade>();
+            var uri = new Uri(pUri);
+            facade.SetupGet(x => x.HostUri).Returns(uri);
+            return facade;
+        }
+
+    }
+}
